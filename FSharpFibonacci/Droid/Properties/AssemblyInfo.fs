98fa36ce568b6df4bb1d9f9a44eb6d72b5a3a88d
Diego Bonilla <diego.bonilla@nareia.com.uy>
2018-06-06 23:23:41 -0300

    fsharp fibonacci

---
+++ FSharpFibonacci/Droid/Properties/AssemblyInfo.fs
@@ -0,0 +1,20 @@
+﻿namespace FSharpFibonacci.Droid.Properties
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<assembly: AssemblyTitle("FSharpFibonacci.Droid")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("")>]
+[<assembly: AssemblyCopyright("(c) Diego Bonilla")>]
+[<assembly: AssemblyTrademark("")>]
+
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+
+[<assembly: AssemblyVersion("1.0.0.0")>]
+
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
+
+()
---
+++ FSharpFibonacci/FSharpFibonacci/App.xaml.fs
@@ -0,0 +1,55 @@
+﻿namespace FSharpFibonacci
+
+open Xamarin.Forms
+open Elmish.XamarinForms  
+open Elmish.XamarinForms.StaticViews
+
+//inmutable
+type Model =  
+    {
+        n : int
+        fn: int
+    }
+
+type Msg =  
+    | Calculate
+    | Setn of int
+
+type App() =
+    inherit Application()
+
+    let init () = 
+        { 
+            n = 0
+            fn = 0
+        }  
+
+    // Fibonacci's function
+    let rec fib n =
+        match n with
+        | 1 | 2 -> 1
+        | n -> fib(n-1) + fib(n-2)
+
+    let update msg model =
+            match msg with
+            | Calculate -> 
+                { 
+                    //new copy of model
+                    fn = fib model.n
+                    n = model.n
+                }
+            | Setn n -> { model with n = n }
+
+    let view () =  
+        MainPage(),
+        [ "fn" |> Binding.oneWay (fun m -> m.fn.ToString())
+          "Calculate" |> Binding.msg Calculate
+          "n" |> Binding.twoWay (fun m -> string m.n) (fun v -> Setn (int (v)))
+        ]
+
+    let runner =  
+        Program.mkSimple init update view
+        |> Program.withStaticView
+        |> Program.run
+
+    do base.MainPage <- runner.InitialMainPage
